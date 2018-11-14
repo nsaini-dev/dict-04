9f09f448cd31bd252eebdc3028faeea904fbd5fc
Neeraj Saini <neeraj.saini.dev@gmail.com>
2018-11-14 13:13:21 +0100

    ADD - added lst_revision_set files

---
+++ database/migrations/2018_11_14_085321_create_lst_revision_sets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLstRevisionSetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lst_revision_sets', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('value', 30)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lst_revision_sets');
+    }
+}
